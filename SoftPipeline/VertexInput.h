8d2ebb91b7f514ff3fa9c1060d771da342ff68cb
Dadio-Xiao <dadio_xiao@163.com>
2020-09-06 15:14:48 +0800

    mesh -> vsInput

---
+++ SoftPipeline/VertexInput.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "glm/glm.hpp"
+struct VertexInput
+{
+	glm::vec4 position;
+	glm::vec4 normals;
+	glm::vec2 uvs;
+};
+
--- SoftPipeline/main.cpp
+++ SoftPipeline/main.cpp
@@ -3,9 +3,13 @@
 #include "BMP.h"
 #include "Mesh.h"
 #include "test.h"
+#include "VertexInput.h"
+#include <vector>
 
 int width = 1920;
 int height = 1080;
+
+void GetVsInputs(const Mesh& mesh, std::vector<VertexInput>& vsInput);
 
 int main()
 {
@@ -15,6 +19,10 @@ int main()
 	mesh.LoadFromFile("Cube.obj");
 
 	//PrintMesh(mesh);
+
+	std::vector<VertexInput> vsInput;
+
+	GetVsInputs(mesh, vsInput);
 	
 	BMP::BMP rt;
 	rt.SetOutPut("renderTarget.bmp", width, height);
@@ -29,4 +37,29 @@ int main()
 	system("pause");
 
 	return 0;
+}
+
+void GetVsInputs(const Mesh & mesh, std::vector<VertexInput>& vsInput)
+{
+	vsInput.resize(mesh.GetIndicesCount());
+
+	auto indices = mesh.GetIndices();
+	auto indicesCount = mesh.GetIndicesCount();
+
+	auto vertices = mesh.GetVerices();
+	auto normals = mesh.GetNormals();
+	auto uvs = mesh.GetUVs();
+
+	int index;
+
+	for (int i = 0; i < indicesCount; i++)
+	{
+		index = indices[i] * 3;
+
+		vsInput[i].position = glm::vec4(vertices[index], vertices[index + 1], vertices[index + 2],1);
+		vsInput[i].normals = glm::vec4(normals[index], normals[index + 1], normals[index + 2],0);
+
+		index = indices[i] * 2;
+		vsInput[i].uvs = glm::vec2(uvs[index], uvs[index + 1]);
+	}
 }
